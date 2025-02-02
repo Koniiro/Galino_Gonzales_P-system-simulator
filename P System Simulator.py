075f3acfa0dcfc705d8175be0f4f771d79356ba9
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-02-02 23:31:12 +0800

    Update P System Simulator.py

--- P System Simulator.py
+++ P System Simulator.py
@@ -21,18 +21,26 @@ while(True):
         object_dict = {obj["type"]: obj["count"] for obj in i["objects"]}
         to_add = {obj["type"]: 0 for obj in i["objects"]}
         for i_r in i["rules"]: #checks which rules may be applied this round
-            object_dict = {obj["type"]: obj["count"] for obj in i["objects"]}
             
             for i_r1 in i_r["input"]:
                 if i_r1["type"] not in object_dict or i_r1["count"] > object_dict[i_r1["type"]]:
                     continue
             valid_rl.append(i_r)
   
-        #while(True):
-        app_rl=valid_rl[random.randint(0, len(valid_rl) - 1)]
-        for i_r1 in app_rl["input"]:
-            if i_r1["type"] not in object_dict or i_r1["count"] > object_dict[i_r1["type"]]:
-                continue
+        while(len(valid_rl)!=0):
+            gacha=random.randint(0, len(valid_rl) - 1)
+            unusable=False	#change to True if rule can no longer be used for popping
+            app_rl=valid_rl[gacha]
+            #test if rule can be applied
+            for i_r1 in app_rl["input"]:	#loop through input reqs
+                if i_r1["type"] not in object_dict or i_r1["count"] > object_dict[i_r1["type"]]:
+                    unusable=True
+                    break
+            if unusable == False:
+                
+            else:
+                valid_rl(gacha) #pop rule if no longer able to apply
+                
                 
         
         print(to_add)
