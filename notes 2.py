98deb929f071b6cbfa3cc04e850e11bf48449b80
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-03-17 00:44:52 +0800

    Delete notes 2.py

--- notes 2.py
+++
@@ -1,8 +0,0 @@
-#Lower bound - DP System (optimally) = >> Lower Bound - simulator with expanded test suite
-#Implement Edits of
-# Work on expansion of simulator over next few weeks
-# expansion of dp systems
-#proof by induction. Use simulator as test forces
-
-# edit gantt chart
-#generate black and white images.
