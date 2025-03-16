2f33ce42dd74189096b65a09a09193a75cb0096a
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-03-17 00:44:21 +0800

    conversion to function
    general reorganization. turned image processing scripts to functions

--- Image Processor.py
+++ Image Processor.py
@@ -1,40 +1,37 @@
 from PIL import Image
 import numpy as np
 
-image_path = "Test-A.png"  # Replace with your image path
-image = Image.open(image_path).convert("L")  # Convert to grayscale
 
 
-threshold = 128  # Midpoint threshold for black/white conversion
-binary_array = np.array(image) < threshold  # True = black, False = white
 
-
-binary_array = np.array(binary_array.astype(int), dtype=np.uint8)
-#3x3
-#nodes=[
-#    [["pw","s0"],["pb","s0"],["pw","s0"]],
-#    [["pb","s0"],["pb","s0"],["pb","s0"]],
-#    [["pw","s0"],["pb","s0"],["pw","s0"]]
-#    ]
-print(binary_array)
-holder=[]
-for i_x in binary_array:
-    temp=[]
-    for i_y in i_x:
-        temp_y=[]
-        if i_y==0:
-            temp_y.append("pw")
-        else:
-            temp_y.append("pb")
-        temp_y.append("s0")
-        temp.append(temp_y)
-    holder.append(temp)
-print(holder)
+def image_proc(input_path,debug):
+    image = Image.open(image_path).convert("L")  # Convert to grayscale
+    threshold = 128  # Midpoint threshold for black/white conversion
+    binary_array = np.array(image) < threshold  # True = black, False = white
+    binary_array = np.array(binary_array.astype(int), dtype=np.uint8)
+    
+    if debug==1:
+        print("Binary NP Array")
+        print(binary_array)
         
+    holder=[]
+    for i_x in binary_array:
+        temp=[]
+        for i_y in i_x:
+            temp_y=[]
+            if i_y==0:
+                temp_y.append("pw")
+            else:
+                temp_y.append("pb")
+            temp_y.append("s0")
+            temp.append(temp_y)
+        holder.append(temp)
+        
+    if debug==1:    
+        print("SCP System Compatible")
+        print(holder)
+        
+    return holder
+image_path = "Test-Cases/Test-A.png"  # Replace with your image path
+print(image_proc(image_path,0))
 
-# image_array = binary_array*255   # 0 -> 0 (black), 1 -> 255 (white)
-# print(image_array.tolist())
-# Create and save the image
-# image = Image.fromarray(image_array, mode="L")  # "L" = grayscale mode
-# image.save("reconstructed_image.png")
-# image.show()  # Opens the image
--- Image Reconstruction.py
+++ Image Reconstruction.py
@@ -3,7 +3,35 @@ import numpy as np
 
 # Example binary data (replace with your actual data)
 
-input=[
+
+
+def image_recon(input_array,debug,image_gen):
+    holder=[]
+    for i_x in input_array:
+        temp=[]
+        for i_y in i_x:
+            
+            if 'pw'in i_y:
+                temp.append(0)
+            elif 'pb' in i_y:
+                temp.append(1)
+          
+            
+        holder.append(temp)
+    binary_array=np.array(holder, dtype=np.uint8)
+    if debug==1:
+        print("Binary NP Array")
+        print(binary_array)
+    if image_gen==1:
+        # # Convert 0s (black) and 1s (white) into 255 grayscale values
+        image_array = binary_array * 255  # 0 -> 0 (black), 1 -> 255 (white)
+        # 
+        # # Create and save the image
+        image = Image.fromarray(image_array, mode="L")  # "L" = grayscale mode
+        image.save("reconstructed_image.png")
+        image.show()  # Opens the image
+        
+nodes=[
 [['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2']],
 [['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pb', 's12', 'h1'], ['s2', 'h1', 'pw'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2']],
 [['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pb', 's12', 'h1'], ['s2', 'h1', 'pw'], ['pb', 's12', 'h1'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2']],
@@ -15,37 +43,4 @@ input=[
 [['s2', 'h1', 'pw'], ['pb', 's12', 'h1'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['s2', 'h1', 'pw'], ['pb', 's12', 'h1']],
 [['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2'], ['pw', 's2']]
 ]
-holder=[]
-for i_x in input:
-    temp=[]
-    for i_y in i_x:
-        
-        if 'pw'in i_y:
-            temp.append(0)
-        elif 'pb' in i_y:
-            temp.append(1)
-      
-        
-    holder.append(temp)
-binary_array=np.array(holder, dtype=np.uint8)
-
-# binary_array = np.array([
-#     [0 0 0 0 0 0 0 0 0 0],
-#  [0 0 0 0 1 1 0 0 0 0],
-#  [0 0 0 1 1 1 0 0 0 0],
-#  [0 0 0 1 0 1 1 0 0 0],
-#  [0 0 1 1 0 0 1 1 0 0],
-#  [0 0 1 1 1 1 1 1 0 0],
-#  [0 1 1 0 1 1 1 1 1 0],
-#  [0 1 1 0 0 0 0 0 1 0],
-#  [1 1 0 0 0 0 0 0 1 1],
-#  [0 0 0 0 0 0 0 0 0 0],
-# ], dtype=np.uint8)
-# 
-# # Convert 0s (black) and 1s (white) into 255 grayscale values
-image_array = binary_array * 255  # 0 -> 0 (black), 1 -> 255 (white)
-# 
-# # Create and save the image
-image = Image.fromarray(image_array, mode="L")  # "L" = grayscale mode
-image.save("reconstructed_image.png")
-image.show()  # Opens the image
+image_recon(nodes,1,0)
