a4a4590ace6d9561dadab22348130a99bdb84184
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-06-03 22:19:31 +0800

    feat: multiprocess implementation

--- SCP_System-G_H-Implementation.py
+++ SCP_System-G_H-Implementation.py
@@ -1,77 +1,89 @@
-from skeletonizer_module import SCP_Skeletonizer
+from skeletonizer_module import multi_proc_skeletonization_handler
 from image_proc_module import image_proc
 from image_recon_module import image_recon
 from log_generator import log_maker
 import time
 import sys
 
-
-# Default values
-file_name = "skl_test.png"
-threshold = 127  # Midpoint is 128
-neg = 0
-#===== inputs =====
-# command line input: python 
-if len(sys.argv) > 1:
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <file_name> <threshold> <negative>")
-        sys.exit(1)
+if __name__ == "__main__":
+    # Default values
+    file_name = "Pasig_City_Seal_Logo.png"  # Input your image file name
+    threshold = 127  						# Midpoint is 128
+    neg = 0									# 1 if negative b/w 0 otherwise
+    multiproc=1 							# 1 if multiprocess 0 otherwise
+    log=0 									# 1 if logs to be made
     
-    file_name = sys.argv[1]
+    img_path = f'../Input-images/{file_name}'  # Replace with your image path
+
+    debug=0
+    bg=0 #1: White BG; 0:Black BG
+    #===== inputs =====
+    # command line input: python
+    # image_path;theshold;negative;log
+    if len(sys.argv) > 1:
+        if len(sys.argv) != 5:
+            print("Usage: python script.py <file_name> <threshold> <negative>")
+            sys.exit(1)
+        
+        file_name = sys.argv[1]
+        
+        try:
+            threshold = int(sys.argv[2])
+            neg = int(sys.argv[3])
+            log = int(sys.argv[4])
+        except ValueError:
+            print("Error: threshold and negative must be integers.")
+            sys.exit(1)
+    assert threshold>=0 or threshold<=255, "threshold should be fro mthe range 0-255"
+    assert neg==0 or neg==1, "neg value should be either 1 or 0"
+    assert log==0 or log==1, "log value should be either 1 or 0"
+
+    img_name=file_name.split(".")[0]
+
+    st=time.time() # Start Timer
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f'Starting Time: {current_time}')
+
+    # ===== Choose Input Image =====
     
-    try:
-        threshold = int(sys.argv[2])
-        neg = int(sys.argv[3])
-    except ValueError:
-        print("Error: threshold and negative must be integers.")
-        sys.exit(1)
-assert threshold>=0 or threshold<=255, "Threshold should be fro mthe range 0-255"
-assert neg==0 or neg==1, "neg should be either 1 or 0"
-
-img_name=file_name.split(".")[0]
-
-st=time.time()
-current_time = time.strftime("%H:%M:%S", time.localtime())
-print(f'Starting Time: {current_time}')
-
-# ===== Choose Input Image =====
-img_path = f'../Input-images/{file_name}'  # Replace with your image path
-debug=0
-bg=0
-
-BW_Image=image_proc(img_path,bg,neg,threshold,debug)
+    BW_Image=image_proc(img_path,bg,neg,threshold,debug)
 
 
-# ===== Reconstruct BW Image =====
-if neg==0:
-    save_pathbw=f'../Output-Images/Ver2/{img_name}-TR{threshold}-BW.png'
-elif neg==1: #negative image
-    save_pathbw=f'../Output-Images/Ver2/{img_name}-TR{threshold}-BW-neg.png'
+    # ===== Reconstruct BW Image =====
+    if neg==0:
+        save_pathbw=f'../Output-Images/Ver2/{img_name}-TR{threshold}-BW.png'
+    elif neg==1: #negative image
+        save_pathbw=f'../Output-Images/Ver2/{img_name}-TR{threshold}-BW-neg.png'
 
-debug=0
-image_gen=1
-image_save=1
-image_recon(BW_Image,debug,image_gen,image_save,save_pathbw)
+    debug_recon=0
+    image_gen=1  #1: Generate an Image; 0 otherwise 
+    image_save=0 #1: Save Image to path; 0 otherwise 
+    image_recon(BW_Image,debug,image_gen,image_save,save_pathbw)
 
-# ===== Conduct Skeletonization ====
-output_states,rnd =SCP_Skeletonizer(BW_Image,1,0)
-#print(output_states)
-
-# ===== Reconstruct SKL Image =====
-if neg==0:
-    save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL.png'
-elif neg==1: #negative image
-    save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg.png'
+    # ===== Conduct Skeletonization ====
+    if multiproc==1:
+        output_states,rnd =multi_proc_skeletonization_handler(BW_Image,0,0)
 
 
-debug=0
-image_gen=1
-image_save=1
+    # ===== Reconstruct SKL Image =====
+    if neg==0:
+        if multiproc==1:
+            save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-multiproc.png'
+        else:
+            save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL.png'
+    elif neg==1: #negative image
+        if multiproc==1:
+            save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg-multiproc.png'
+        else:
+            save_pathproc=f'../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg.png'
+            
+    image_recon(output_states,debug_recon,image_gen,image_save,save_pathproc)
+    
+    et = time.time()
+    elapsed=et-st
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f'End Time: {current_time}')
+    print('Execution time:', elapsed, 'seconds')
 
-image_recon(output_states,debug,image_gen,image_save,save_pathproc)
-et = time.time()
-elapsed=et-st
-current_time = time.strftime("%H:%M:%S", time.localtime())
-log_maker(file_name,threshold,st,et,elapsed,rnd,neg)
-print(f'End Time: {current_time}')
-print('Execution time:', elapsed, 'seconds')
+    if log==1:
+        log_maker(file_name,threshold,st,et,elapsed,rnd,neg,multiproc)
--- skeletonizer_module.py
+++ skeletonizer_module.py
@@ -20,17 +20,7 @@ functions = {
     "rule_13": rule_13,
     "rule_14": rule_14
 }
-def SCP_Skeletonizer(nodes,debug_print,quadrant):
-    #print(quadrant)
-    count=0
-    #print(len(nodes))
-    #print(len(nodes[0]))
-    for i in nodes:
-        for i_c in i:
-            #print(i_c)
-            count+=1
-    #print("======")
-    return count
+
 
 def multi_proc_skeletonization_handler(nodes,verbose,rule_debug):
     print("Initializing Multiprocessed Skeletonization")
@@ -114,77 +104,13 @@ def mult_proc_skeletonizer(nodes, neighbors,label,rule_debug):
 
     return nodes,checksum
 
-        
-    
-#     active=1
-#     check=0
-#     checksum=0
-#     rnd=1
-#     # Print Initial Configuration
-#     if verbose==1:
-#         print("==Initial Configuration==")
-#         for x in nodes:
-#             print(x)
-#     while(True):
-#         #if verbose==1:
-#         print(f'Round {rnd}')
-#     
-#         
-# #     for name, func in functions.items():
-# #         if rule_debug==1:
-# #             print(f"Executing {name}:")
-# #         neyb=neighbor_gen(nodes)
-# #         for i_x in range(len(nodes)):
-# #            
-# #             for i_y in range(len(nodes[i_x])):
-# #                 if name=="rule_11":
-# #                     nodes[i_x][i_y],check=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
-# #                     checksum+=check
-# #                     
-# #                 else:
-# #                     nodes[i_x][i_y]=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
-# #             if rule_debug==1:
-# #                 print(nodes[i_x][i_y])
-# #                 print("---------")
-#             
-# #         if checksum==0: # indicates if rule 11 was run
-# #             break
-# #         checksum=0
-# #         rnd+=1
-#     print("=====Finished=====")
-#     print(f'Rounds Taken: {rnd}')
-#     if verbose==1:
-#         print("Final Configuration")
-#     final_arr=[]
-#     for x in nodes:
-#         if verbose==1:
-#             print(x)
-#         final_arr.append(x)
-#     return final_arr, rnd
 
 
-def mult_proc_skeletonizerTest(nodes,debug):
-    print("Initializing Multiprocessed Skeletonization Test")
-    processes = []
-    quadrant = ['Q1', 'Q2', 'Q3', 'Q4']
-    args_list = [(nodes[q],0,quadrant[q]) for q in range(4)]
 
-    with multiprocessing.Pool(processes=1) as pool:
-        counts = pool.starmap(SCP_Skeletonizer, args_list)
-    total_count = sum(counts)
-    print(f"Total count from all processes: {total_count}")
-
-def single_proc_skeletonizer(nodes,debug):
-    print("Initializing Single Process Skeletonization Test")
-    count=0
-    for i in nodes:
-        for i_c in i:
-            count+=1;
-    print(f'Cells: {count}')
 
 
 if __name__ == "__main__":
-    img_route='Pasig_City_Seal_Logo.png'
+    img_route='4x5_test.png'
     NUM_PROCESSES = 4
   
     image_path = f'../Input-images/{img_route}'  # Replace with your image path
@@ -195,29 +121,16 @@ if __name__ == "__main__":
 #         print(i)
     
     start_time = time.time()
-    output_array,rnd=multi_proc_skeletonization_handler(rawImgMat,0,0)
+    output_array,rnd=multi_proc_skeletonization_handler(rawImgMat,1,0)
     end_time = time.time()
     elapsed = end_time - start_time
     print(f"Finished with {NUM_PROCESSES} processes in {elapsed:.2f} seconds in {rnd} rounds")
-    debug=0
-    image_gen=1
-    image_save=0
-    image_recon(output_array,debug,image_gen,image_save,'')
+    #debug=0
+    #image_gen=1
+    #image_save=0
+    #image_recon(output_array,debug,image_gen,image_save,'')
     
     
     
-    
-    
-#     quad_arr=quadrant_gen(rawImgMat,0)
-#     start_time = time.time()
-#     mult_proc_skeletonizerTest(quad_arr,0)
-#     end_time = time.time()
-#     elapsed = end_time - start_time
-#     print(f"Finished with 1 process in {elapsed:.2f} seconds")
-#     
-#     start_time1 = time.time()
-#     single_proc_skeletonizer(rawImgMat,0)
-#     end_time2 = time.time()
-#     elapsed2 = end_time2 - start_time1
-#     print(f"Finished with 1 process in {elapsed2:.2f} seconds")
+
 
