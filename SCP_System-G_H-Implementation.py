c316bbfc852f411ea8ce5de09b087b574c048b24
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-06-06 01:35:55 +0800

    refactor: separation of skeletonizer module to separate file

--- SCP_System-G_H-Implementation.py
+++ SCP_System-G_H-Implementation.py
@@ -1,73 +1,13 @@
-from rule_module import rule_01, rule_02, rule_03, rule_05, rule_06, rule_08, rule_09, rule_10, rule_11, rule_13, rule_14
-from neighbor_gen_module import neighbor_gen
+from skeletonizer_module import SCP_Skeletonizer
 from image_proc_module import image_proc
 from image_recon_module import image_recon
 from log_generator import log_maker
 import time
 import sys
-functions = {
-    "rule_01": rule_01,
-    "rule_02": rule_02,
-    "rule_03": rule_03,
-    "rule_05": rule_05,
-    "rule_06": rule_06,
-    "rule_08": rule_08,
-    "rule_09": rule_09,
-    "rule_10": rule_10,
-    "rule_11": rule_11,
-    "rule_13": rule_13,
-    "rule_14": rule_14
-}
 
 
 
-def SCP_Skeletonizer(nodes,verbose,rule_debug):
-    active=1
-    check=0
-    checksum=0
-    rnd=1
-    # Print Initial Configuration
-    if verbose==1:
-        print("==Initial Configuration==")
-        for x in nodes:
-           print(x)
-                
 
-    while(True):
-        #if verbose==1:
-        print(f'Round {rnd}')
-        
-        for name, func in functions.items():
-            if rule_debug==1:
-                print(f"Executing {name}:")
-            neyb=neighbor_gen(nodes)
-            for i_x in range(len(nodes)):
-               
-                for i_y in range(len(nodes[i_x])):
-                    if name=="rule_11":
-                        nodes[i_x][i_y],check=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
-                        checksum+=check
-                        
-                    else:
-                        nodes[i_x][i_y]=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
-                if rule_debug==1:
-                    print(nodes[i_x][i_y])
-                    print("---------")
-            
-        if checksum==0:
-            break
-        checksum=0
-        rnd+=1
-    print("=====Finished=====")
-    print(f'Rounds Taken: {rnd}')
-    if verbose==1:
-        print("Final Configuration")
-    final_arr=[]
-    for x in nodes:
-        if verbose==1:
-            print(x)
-        final_arr.append(x)
-    return final_arr, rnd
 
 #===== inputs =====
 # command line input: python 
@@ -107,7 +47,7 @@ elif neg==1: #negative image
 
 debug=0
 image_gen=1
-image_save=1
+image_save=0
 image_recon(BW_Image,debug,image_gen,image_save,save_pathbw)
 
 # ===== Conduct Skeletonization ====
---
+++ skeletonizer_module.py
@@ -0,0 +1,64 @@
+from neighbor_gen_module import neighbor_gen
+from rule_module import rule_01, rule_02, rule_03, rule_05, rule_06, rule_08, rule_09, rule_10, rule_11, rule_13, rule_14
+
+functions = {
+    "rule_01": rule_01,
+    "rule_02": rule_02,
+    "rule_03": rule_03,
+    "rule_05": rule_05,
+    "rule_06": rule_06,
+    "rule_08": rule_08,
+    "rule_09": rule_09,
+    "rule_10": rule_10,
+    "rule_11": rule_11,
+    "rule_13": rule_13,
+    "rule_14": rule_14
+}
+
+def SCP_Skeletonizer(nodes,verbose,rule_debug):
+    active=1
+    check=0
+    checksum=0
+    rnd=1
+    # Print Initial Configuration
+    if verbose==1:
+        print("==Initial Configuration==")
+        for x in nodes:
+           print(x)
+                
+
+    while(True):
+        #if verbose==1:
+        print(f'Round {rnd}')
+        
+        for name, func in functions.items():
+            if rule_debug==1:
+                print(f"Executing {name}:")
+            neyb=neighbor_gen(nodes)
+            for i_x in range(len(nodes)):
+               
+                for i_y in range(len(nodes[i_x])):
+                    if name=="rule_11":
+                        nodes[i_x][i_y],check=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
+                        checksum+=check
+                        
+                    else:
+                        nodes[i_x][i_y]=func(nodes[i_x][i_y],i_x,i_y,neyb[i_x][i_y],active)
+                if rule_debug==1:
+                    print(nodes[i_x][i_y])
+                    print("---------")
+            
+        if checksum==0:
+            break
+        checksum=0
+        rnd+=1
+    print("=====Finished=====")
+    print(f'Rounds Taken: {rnd}')
+    if verbose==1:
+        print("Final Configuration")
+    final_arr=[]
+    for x in nodes:
+        if verbose==1:
+            print(x)
+        final_arr.append(x)
+    return final_arr, rnd
