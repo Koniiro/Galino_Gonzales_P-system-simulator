d15128f79c3cac0ff373821383f4d236fa0a4c91
John Henry Galino <git@jhgalino.com>
2025-06-08 15:14:02 +0000

    Use argparse instead of sys.argv
    Make cli use argparse instead of checking sys.argv manually

--- SCP_System-G_H-Implementation.py
+++ SCP_System-G_H-Implementation.py
@@ -3,46 +3,68 @@ from image_proc_module import image_proc
 from image_recon_module import image_recon
 from log_generator import log_maker
 import time
-import sys
+import argparse
+import os
+from typing import Any
 
 if __name__ == "__main__":
-    # Default values
-    file_name = "LTEST-0015.png"  # Input your image file name
-    threshold = 115  # Midpoint is 128
-    neg = 0  # 1 if negative b/w 0 otherwise
-    multiproc = 1  # 1 if multiprocess 0 otherwise
-    log = 1  # 1 if logs to be made
-
-    img_path = (
-        f"../Input-images/medium_image_test/{file_name}"  # Replace with your image path
+    parser = argparse.ArgumentParser()
+    _ = parser.add_argument("path", help="path of image to skeletonize")
+    _ = parser.add_argument(
+    "-t", "--threshold", help="threshold for binarization. must be 0 to 255", type=int
     )
+    _ = parser.add_argument(
+        "-n", "--negative", help="skeletonize the inverse of the image", action="store_true"
+    )
+    _ = parser.add_argument("-m", "--multiproc", help="use multiprocessing", action="store_true")
+    _ = parser.add_argument("-l", "--logging", help="use logging", action="store_true")
+    _ = parser.add_argument("-d", "--debug", help="print debugging messages")
+    _ = parser.add_argument("-bg", "--background", help="background of the image, 1 for white and 0 for black", type=int)
+    args = parser.parse_args()
+    # Default values
+    # file_name = "LTEST-0015.png"  # Input your image file name
+    # threshold = 115  # Midpoint is 128
+    # neg = 0  # 1 if negative b/w 0 otherwise
+    # multiproc = 1  # 1 if multiprocess 0 otherwise
+    # log = 1  # 1 if logs to be made
 
-    debug = 0
-    bg = 0  # 1: White BG; 0:Black BG
+    # img_path = (
+    #     f"../Input-images/medium_image_test/{file_name}"  # Replace with your image path
+    # )
+
+    # debug = 0
+    if not args.debug:
+        args.debug = 0
+    
+    # bg = 0  # 1: White BG; 0:Black BG
+    if not args.background:
+        args.background = 0
     # ===== inputs =====
     # command line input: python
     # image_path;theshold;negative;log
-    if len(sys.argv) > 1:
-        if len(sys.argv) != 5:
-            print("Usage: python script.py <file_name> <threshold> <negative>")
-            sys.exit(1)
+    # if len(sys.argv) > 1:
+    #     if len(sys.argv) != 5:
+    #         print("Usage: python script.py <file_name> <threshold> <negative>")
+    #         sys.exit(1)
 
-        file_name = sys.argv[1]
+    #     file_name = sys.argv[1]
 
-        try:
-            threshold = int(sys.argv[2])
-            neg = int(sys.argv[3])
-            log = int(sys.argv[4])
-        except ValueError:
-            print("Error: threshold and negative must be integers.")
-            sys.exit(1)
-    assert threshold >= 0 or threshold <= 255, (
-        "threshold should be fro mthe range 0-255"
-    )
-    assert neg == 0 or neg == 1, "neg value should be either 1 or 0"
-    assert log == 0 or log == 1, "log value should be either 1 or 0"
+    #     try:
+    #         threshold = int(sys.argv[2])
+    #         neg = int(sys.argv[3])
+    #         log = int(sys.argv[4])
+    #     except ValueError:
+    #         print("Error: threshold and negative must be integers.")
+    #         sys.exit(1)
+    # assert threshold >= 0 or threshold <= 255, (
+    #     "threshold should be fro mthe range 0-255"
+    # )
+    # assert neg == 0 or neg == 1, "neg value should be either 1 or 0"
+    # assert log == 0 or log == 1, "log value should be either 1 or 0"
+    assert 0 <= args.threshold <= 255, "Threshold should be in 0-255"
 
-    img_name = file_name.split(".")[0]
+    img_name = os.path.basename(args.path)
+    # img_name = file_name.split(".")[0]
 
     st = time.time()  # Start Timer
     current_time = time.strftime("%H:%M:%S", time.localtime())
@@ -50,40 +72,48 @@ if __name__ == "__main__":
 
     # ===== Choose Input Image =====
 
-    BW_Image = image_proc(img_path, bg, neg, threshold, debug)
+    BW_Image = image_proc(args.path, args.background, args.negative, args.threshold, args.debug)
 
     # ===== Reconstruct BW Image =====
-    if neg == 0:
-        save_pathbw = f"../Output-Images/Ver2/{img_name}-TR{threshold}-BW.png"
-    elif neg == 1:  # negative image
-        save_pathbw = f"../Output-Images/Ver2/{img_name}-TR{threshold}-BW-neg.png"
+    if 'output-images' not in os.listdir():
+        os.mkdir('output-images')
+    save_pathbw = f'output-images/{img_name}-TR{args.threshold}-BW-neg.png' if args.negative else f'output-images/{img_name}-TR{args.threshold}-BW.png'
+    # if neg == 0:
+    #     save_pathbw = f"../Output-Images/Ver2/{img_name}-TR{threshold}-BW.png"
+    # elif neg == 1:  # negative image
+    #     save_pathbw = f"../Output-Images/Ver2/{img_name}-TR{threshold}-BW-neg.png"
 
     debug_recon = 0
     image_gen = 1  # 1: Generate an Image; 0 otherwise
     image_save = 1  # 1: Save Image to path; 0 otherwise
-    image_recon(BW_Image, debug, image_gen, image_save, save_pathbw)
+    image_recon(BW_Image, args.debug, image_gen, image_save, save_pathbw)
 
     # ===== Conduct Skeletonization ====
-    if multiproc == 1:
-        output_states, rnd = multi_proc_skeletonization_handler(BW_Image, 0, 0)
+    # if multiproc == 1:
+    output_states, rnd = multi_proc_skeletonization_handler(BW_Image, 0, 0)
 
     # ===== Reconstruct SKL Image =====
-    if neg == 0:
-        if multiproc == 1:
-            save_pathproc = (
-                f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-multiproc.png"
-            )
-        else:
-            save_pathproc = f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL.png"
-    elif neg == 1:  # negative image
-        if multiproc == 1:
-            save_pathproc = (
-                f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg-multiproc.png"
-            )
-        else:
-            save_pathproc = (
-                f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg.png"
-            )
+    # if neg == 0:
+        # if multiproc == 1:
+    save_pathproc: str
+    if not args.negative:
+        save_pathproc = f'output-images/{img_name}-TR{args.threshold}-SKL-multiproc.png'
+        #     save_pathproc = (
+        #         f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-multiproc.png"
+        #     )
+        # else:
+        #     save_pathproc = f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL.png"
+    # elif neg == 1:  # negative image
+    #     if multiproc == 1:
+    #         save_pathproc = (
+    #             f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg-multiproc.png"
+    #         )
+    #     else:
+    #         save_pathproc = (
+    #             f"../Output-Images/Ver2/{img_name}-TR{threshold}-SKL-neg.png"
+    #         )
+    else:
+        save_pathproc = f'output-images/{img_name}-TR{args.threshold}-SKL-neg-multiproc.png' 
 
     image_recon(output_states, debug_recon, image_gen, image_save, save_pathproc)
     length = len(output_states)
@@ -95,16 +125,17 @@ if __name__ == "__main__":
     print(f"End Time: {current_time}")
     print("Execution time:", elapsed, "seconds")
 
-    if log == 1:
+    if args.logging:
+    # if log == 1:
         log_maker(
-            file_name,
-            threshold,
+            args.path,
+            args.threshold,
             st,
             et,
             elapsed,
             rnd,
-            neg,
-            multiproc,
+            args.negative,
+            args.multiproc,
             length,
             width,
             pixel_count,
