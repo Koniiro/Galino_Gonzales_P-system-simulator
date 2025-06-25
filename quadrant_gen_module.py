5e23c7f1fb5dca065d332d86b81e15e1bfca98c7
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-06-26 01:21:04 +0800

    perf: update quadrant gen to numpy

--- quadrant_gen_module.py
+++ quadrant_gen_module.py
@@ -1,70 +1,61 @@
 from image_proc_module import image_proc
+import time
 
 
 def quadrant_gen(rawImageData, debug):
-    quadrant_raw = [[[], []], [[], []]]
-    quadrant = []
-    colCoord = [0]
-    rowCoord = [0]
     colFinal = len(rawImageData[0]) - 1  # Gets last index for coloumns
     rowFinal = len(rawImageData) - 1  # Gets last index for rows
-    colCoord.append(round((colFinal - 1) / 2))
-    colCoord.append(round((colFinal - 1) / 2) + 1)
-    colCoord.append(colFinal)
+    colCoord = [0,round((colFinal - 1) / 2),round((colFinal - 1) / 2) + 1,colFinal]
+    rowCoord = [0,round((rowFinal - 1) / 2),round((rowFinal - 1) / 2) + 1,rowFinal]
 
-    rowCoord.append(round((rowFinal - 1) / 2))
-    rowCoord.append(round((rowFinal - 1) / 2) + 1)
-    rowCoord.append(rowFinal)
     if debug == 1:
         print("Image Index coord")
         print(colCoord)
         print(rowCoord)
+        row_print(rawImageData)
 
-    qRow = 0  # r,c
+    q0=rawImageData[:rowCoord[1]+1,:colCoord[1]+1]
+    q1=rawImageData[:rowCoord[1]+1,colCoord[2]:,:colCoord[3]+1]
+    q2=rawImageData[rowCoord[2]:rowCoord[3]+1,:colCoord[1]+1]
+    q3=rawImageData[rowCoord[2]:rowCoord[3]+1,colCoord[2]:,:colCoord[3]+1]
+   
+    quadrant=[q0,q1,q2,q3]
 
-    for i_r in range(0, len(rawImageData)):
-        lSeg = []
-        rSeg = []
-        if i_r >= rowCoord[2]:
-            qRow = 1
-        else:
-            qRow = 0
-        row = rawImageData[i_r]
-
-        for i_c in range(0, len(row)):
-            cell = row[i_c]
-            if i_c >= colCoord[2]:
-                rSeg.append(cell)
-            else:
-                lSeg.append(cell)
-        quadrant_raw[qRow][0].append(lSeg)
-        quadrant_raw[qRow][1].append(rSeg)
-    quadrant.append(quadrant_raw[0][0])
-    quadrant.append(quadrant_raw[0][1])
-    quadrant.append(quadrant_raw[1][0])
-    quadrant.append(quadrant_raw[1][1])
 
     return quadrant
 
-
+def row_print(quad):
+    for ir in  quad:
+        for i in ir:
+            print(i , end=",")
+        print("")
+def quadrant_print(quad):
+    for i in range(0, len(quad), 2):
+        for i_r in range(len(quad_arr[i])):
+            print(f"{quad_arr[i][i_r].tolist()} || {quad_arr[i + 1][i_r].tolist()}")
+        print("=========================================")
 if __name__ == "__main__":
     img_route = "4x5_test.png"
-    image_path = f"../Input-images/{img_route}"  # Replace with your image path
-    # row
+    #image_path = f"../Input-images/{img_route}"  # Replace with your image path
+    #image_path = f"../Input-images/large_image_test/LTEST-0006.png"
+    debug=0
 
     rawImgMat = image_proc(image_path, 0, 0, 50, 0)
-    print("Raw Img Nodes:")
-    for i in rawImgMat:
-        print(i)
-    quad_arr = quadrant_gen(rawImgMat, 0)
-    print("Quad Array Raw:")
-    print(quad_arr)
-    print("Quad Array Organized:")
-    for i in range(int(len(quad_arr))):
-        print(f"Quadrant {i}:")
-        print(quad_arr[i])
-    print("Quadrant Diagram")
-    for i in range(0, len(quad_arr), 2):
-        for i_r in range(len(quad_arr[i])):
-            print(f"{quad_arr[i][i_r]}||{quad_arr[i + 1][i_r]}")
-        print("=========================================")
+
+    st1 = time.time()  # Start Timer
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f"Starting Time: {current_time}")
+
+    quad_arr = quadrant_gen(rawImgMat, debug)
+    
+    et1 = time.time()
+    elapsed = et1 - st1
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f"End Time: {current_time}")
+    print("Execution time:", elapsed, "seconds")
+    
+    if debug==1:
+        print("Quad Array:")
+        quadrant_print(quad_arr)
+        
+    
