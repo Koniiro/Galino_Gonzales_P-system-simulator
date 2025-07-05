cbd07bbee1cb3206eb63fc7567af8793933509d3
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-07-06 02:05:55 +0800

    feat: Optimization of Neighbor generation function
    use of pad function and sliding_window_view to speed up generation

--- neighbor_gen_module.py
+++ neighbor_gen_module.py
@@ -1,81 +1,86 @@
 from image_proc_module import image_proc
 from quadrant_gen_module import quadrant_gen
-
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+import time
 
 def neighbor_proc(i, j):
-    return [
-        (i - 1, j - 1),  # Upper-left
-        (i - 1, j),  # Up
-        (i - 1, j + 1),  # Upper-right
-        (i, j + 1),  # Right
-        (i + 1, j + 1),  # Bottom-right
-        (i + 1, j),  # Down
-        (i + 1, j - 1),  # Bottom-left
-        (i, j - 1),  # Left
-    ]
+    i= np.array([
+        i - 1,  # Upper-left
+        i - 1,  # Up
+        i - 1,  # Upper-right
+        i,  # Right
+        i + 1,  # Bottom-right
+        i + 1,  # Down
+        i + 1,  # Bottom-left
+        i,  # Left
+    ])
+    j=np.array([
+         j - 1,  # Upper-left
+         j,  # Up
+         j + 1,  # Upper-right
+         j + 1,  # Right
+         j + 1,  # Bottom-right
+         j,  # Down
+         j - 1,  # Bottom-left
+         j - 1,  # Left
+    ])
+    return i,j
 
 
-def neighbor_gen(nodes):
-    temp_holder = []
-
-    for i_x in range(len(nodes)):
-        row = []
-        for i_y in range(len(nodes[i_x])):
-            hold = []
-            # Append Target Node's color
-            if "pb" in nodes[i_x][i_y]:
-                hold.append("pb")
-            elif "pw" in nodes[i_x][i_y]:
-                hold.append("pw")
-            loc_neigh = neighbor_proc(i_x, i_y)
-
-            # Append Target Node neighbor's colors clockwise from upperleft
-            for x, y in loc_neigh:
-                # If out side of bounds, is white
-                if x < 0 or y < 0 or x > len(nodes) - 1 or y > len(nodes[0]) - 1:
-                    hold.append("pw")
-                else:
-                    if "pb" in nodes[x][y]:
-                        hold.append("pb")
-                    elif "pw" in nodes[x][y]:
-                        hold.append("pw")
-            # hold.append(f'{i_x},{i_y}')
-            row.append(hold)
-        temp_holder.append(row)
-
-    return temp_holder
+def neighbor_gen_2(window,x,y):
+   
+#     flat_nodes = np.ravel(nodes)
+#     filtered = flat_nodes[np.isin(flat_nodes, ['pw', 'pb'])]
+#     node_test = filtered.reshape(len(rawImgMat), len(rawImgMat[0]))
+#     padded = np.pad(node_test, pad_width=1, mode='constant', constant_values=np.array('pw'))
+#     windows = sliding_window_view(padded, (3, 3))
+    target=window[x][y]
+    neighbor_array=np.array([target[1][1],target[0][0],target[0][1],target[0][2],target[1][2],target[2][2],target[2][1],target[2][0],target[1][0]])
+  
+      
+   
+    return neighbor_array
 
 
 if __name__ == "__main__":
-    img_route = "4x5_test.png"
+    img_route = "/irl_test/irl_test-01.jpg"
     image_path = f"../Input-images/{img_route}"  # Replace with your image path
-
     rawImgMat = image_proc(image_path, 0, 0, 50, 0)
+    mask = np.isin(rawImgMat, ['pw', 'pb'])
+#     for i_x in rawImgMat:
+#         for i_y in i_x:
+#             print(i_y,end="")
+#         print("")
+    rawImgMat = np.array(rawImgMat)
+    rawImgMat=rawImgMat.astype(np.str_)
 
-    neighbor_nodesRaw = neighbor_gen(
-        rawImgMat
-    )  # Neighbor Nodes on per row: [row:[cell-neighbor:[]...],...,row:[]]
+    start_time = time.time()
 
-    neighbor_quad = quadrant_gen(neighbor_nodesRaw, 0)
+    # Run your function
+    print("Beginning Neighbor Gen")
+    flat_nodes = np.ravel(rawImgMat)
+    filtered = flat_nodes[np.isin(flat_nodes, ['pw', 'pb'])]
+    node_test = filtered.reshape(len(rawImgMat), len(rawImgMat[0]))
+    padded = np.pad(node_test, pad_width=1, mode='constant', constant_values=np.array('pw'))
+    windows = sliding_window_view(padded, (3, 3))
+#     neighbor_nodes = neighbor_gen(
+#         rawImgMat,
+#         1
+#     )  # Neighbor Nodes on per row: [row:[cell-neighbor:[]...],...,row:[]]
+    for i_x in range(len(rawImgMat)):
+        for i_y in range(len(rawImgMat[0])):
+            neighbor_nodes =neighbor_gen_2(windows,i_x,i_y)
+    
+    # End timer
+    end_time = time.time()
 
-    quad_arr = quadrant_gen(rawImgMat, 0)
-    print(len(neighbor_nodesRaw))
+    # Calculate and print elapsed time
+    elapsed = end_time - start_time
+    print(f"Function took {elapsed:.4f} seconds")
+   # neighbor_nodes=neighbor_nodes.astype(str)
 
-    print("Quadrant Diagram")
-    for i in range(0, len(quad_arr), 2):
-        for i_r in range(len(quad_arr[i])):
-            print(f"{quad_arr[i][i_r]}||{quad_arr[i + 1][i_r]}")
-        print("=========================================")
 
-#     for i_quadr in range(len(neighbor_quad)):
-#         print(f'Quad Row {i_quadr}:')
-#         print(f'Rows: {len(neighbor_quad[i_quadr])}; Cols: {len(neighbor_quad[i_quadr][0])}\n{neighbor_quad[i_quadr]}')
-#     q=2
-#     print(f'Quadrant {q}:')
-#
-#     for i_r in range(len(quad_arr[q])):
-#         print(f'{quad_arr[q][i_r]}')
-#
-#     print(f'Rows: {len(neighbor_quad[q])}; Cols: {len(neighbor_quad[q][0])}')
-#     for i_nquad in range(len(neighbor_quad[q])):
-#         print(f'{neighbor_quad[q][i_nquad]}')
+    
+
+    print("Neighbor Generation Successful")
